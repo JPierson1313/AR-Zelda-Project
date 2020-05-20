fb8fc7b2f079319b0ad582e69cfcbaac03f79b48
JPierson1313 <jpierso3@masonlive.gmu.edu>
2020-05-19 23:00:29 -0400

    Added final details to project

---
+++ ARWindow/Assets/Scripts/RayCastButtonScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastButtonScript : MonoBehaviour
+{
+    string btnName;
+    public GameObject mainScreenItems;
+    public GameObject map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                btnName = hit.transform.name;
+                switch (btnName)
+                {
+                    case "InfoButton":
+                        mainScreenItems.SetActive(false);
+                        map.SetActive(true);
+                        break;
+
+                    case "BackButton":
+                        Debug.Log("Check");
+                        map.GetComponent<MapScaleScript>().canIncrease = false;
+                        break;
+                }
+            }
+        }
+    }
+}
