bf0928bdbc0bb2dd743e24e3b9ac524acf3a4111
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-09 16:15:41 -0600

    Update MapScaleScript.cs

--- ARWindow/Assets/Scripts/MapScaleScript.cs
+++ ARWindow/Assets/Scripts/MapScaleScript.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class MapScaleScript : MonoBehaviour
 {
-    public bool canIncrease;
-    public GameObject mainScreenItems;
+    public bool canIncrease; //Boolean used to see if we can increase the size of the map or not
+    public GameObject mainScreenItems; //Collection of game objects that are used for the Main Screen
 
-    private void OnEnable()
+    void Update()
     {
-        canIncrease = true;
-       
-    }
-
-    private void OnDisable()
-    {
-        transform.localScale += new Vector3(0, 0, 0);
-    }
-    private void Update()
-    {
+        //If canIncrease is true, then we add 0.05 to both the x and y local scale until they are greater than 0.5 in the x and y local scale
         if(canIncrease)
         {
             if (transform.localScale.x < 0.5f && transform.localScale.y < 0.5f)
@@ -26,7 +17,9 @@ public class MapScaleScript : MonoBehaviour
                 transform.localScale += new Vector3(0.05f, 0.05f, 0);
             }
         }
-
+        
+        //If canIncrease is false, then we add -0.05 to both the x and y local scale until they are less than 0 in the x and y local scale
+        //When the local scale of both x and y are less than 0, then the mainScreenItems game object will be active and the Map game object will be inactive
         if(!canIncrease)
         {
             transform.localScale += new Vector3(-0.05f, -0.05f, 0);
