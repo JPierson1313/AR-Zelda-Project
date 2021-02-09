7c0bd69792dc30a9319cb91e5fc83965874c6492
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-09 16:22:39 -0600

    Update RayCastButtonScript.cs

--- ARWindow/Assets/Scripts/RayCastButtonScript.cs
+++ ARWindow/Assets/Scripts/RayCastButtonScript.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class RayCastButtonScript : MonoBehaviour
 {
-    string btnName;
-    public GameObject mainScreenItems;
-    public GameObject map;
+    string btnName; //The name of the button
+    public GameObject mainScreenItems; //Collection of game objects for the Main Screen
+    public GameObject map; //The map for the information about the class
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,7 @@ public class RayCastButtonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //When a user taps/clicks on the screen, a raycast is use to check to see if they hit any buttons that are named and listed in the switch function
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -27,12 +28,15 @@ public class RayCastButtonScript : MonoBehaviour
                 btnName = hit.transform.name;
                 switch (btnName)
                 {
+                    //Case for if the user clicks on a button name InfoButton, then it will tell mainScreenItems to be inactive, map to be active,
+                    //and tell the CanIncrease boolean from MapScaleScript to be true
                     case "InfoButton":
                         mainScreenItems.SetActive(false);
                         map.SetActive(true);
                         map.GetComponent<MapScaleScript>().canIncrease = true;
                         break;
-
+                    
+                    //Case for if the user clicks on a button name BackButton, then it will tell the CanIncrease boolean from MapScaleScript to be false
                     case "BackButton":
                         Debug.Log("Check");
                         map.GetComponent<MapScaleScript>().canIncrease = false;
